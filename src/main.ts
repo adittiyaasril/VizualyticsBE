3268d77ff83615ecf81b9ceaa454c841ca183ca3
adittiyaasril <adittiyaasril369@gmail.com>
2023-09-19 11:36:18 +0700

    chore: vercel.json

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
