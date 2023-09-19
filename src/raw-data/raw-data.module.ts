9518d30e6526b0a174a086eb7aa05f870b2cefc7
adittiyaasril <adittiyaasril369@gmail.com>
2023-09-19 12:46:42 +0700

    fix:

--- src/raw-data/raw-data.module.ts
+++ src/raw-data/raw-data.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RawDataController } from './raw-data.controller';
 import { RawDataService } from './raw-data.service';
 import { rawDataProviders } from './raw-data.provider';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
